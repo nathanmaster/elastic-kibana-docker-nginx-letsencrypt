c595d9893fcda0477f57a76a4f435a4ff6a676e6
Nathan <35269855+nathanmaster@users.noreply.github.com>
2025-05-13 15:29:24 -0400

    Update Elasticsearch and Kibana versions, modify domain names in configuration files

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(DOMAINNAME.com)
+domains=(kibana.edgecomsec.info)
 rsa_key_size=4096
 data_path="./nginx/certbot"
-email="example@example.com" # Adding a valid address is strongly recommended
+email="ns122463@gmail.com" # Updated email address
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
